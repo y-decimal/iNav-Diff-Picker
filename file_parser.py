298afc8381ecb1169ef42ab05e1390fdd6606992
Decimal <83334510+y-decimal@users.noreply.github.com>
2026-05-21 14:12:08 +0200

    feat: add FileParser class for parsing text files and detecting diffs chore: add test diff files to gitignore

---
+++ file_parser.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+class FileParser:
+    
+    diff_files = list()
+    source_dir = Path("")
+    
+    def __init__(self, source_dir):
+        self.source_dir = Path(source_dir)
+
+    def parse_files(self):
+        txt_files = list(self.source_dir.glob("*.txt"))
+        for txt_file in txt_files:
+            with open(txt_file, "r") as f:
+                content = f.read()
+                if "diff" in content:
+                    print(f"Found diff in {txt_file.name}")
+                    self.diff_files.append(txt_file)
+
+    def get_diff_files(self):
+        return self.diff_files
