619afc33087039768997adaaaa3f620d6c08bf7a
Decimal <83334510+y-decimal@users.noreply.github.com>
2026-05-23 00:21:16 +0200

    fix: fix parsing logic log: add logging for debugging

--- diff_parser.py
+++ diff_parser.py
@@ -5,21 +5,33 @@ class DiffParser:
     def __init__(self, file_path):
         self.file_path = file_path
         self.diffs = []
+        self.debug = False
 
     def parse_diffs(self):
         with open(self.file_path, "r") as f:
             content = f.read()
-            index = 0
             diff_block = []
             for line in content.splitlines():
                 if line.startswith("#"):
-                    if (diff_block):
-                        self.diffs.append(diff_block)
-                    diff_block = []
-                    diff_block.append(line)
-                elif line.strip() == "":
                     if diff_block:
                         self.diffs.append(diff_block)
+                        if self.debug:
+                            print("Added diff block to diffs at index      #", len(self.diffs) - 1, " :", diff_block[0])
+                            print("------------------------ End diff block #", len(self.diffs) - 1, "------------------------")
+                    diff_block = []
+                    diff_block.append(line)
+                    if self.debug:
+                        print("Added header to current diff block      #", len(self.diffs), " :", line)
+                elif line.strip() == "":
+                    if self.debug:
+                        print("--------------------- Found empty line, skipping ---------------------")
+                    continue
+                elif diff_block:
+                    diff_block.append(line)
+                    if self.debug:
+                        print("Added line to current diff block        #", len(self.diffs), " :", line)
+
+            # the diff file ends with an empty header line, so we do not need to add the last diff block
 
     def get_diffs(self):
         return self.diffs
