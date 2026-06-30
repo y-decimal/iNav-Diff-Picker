858bad20e7ad44a63115ac50196db429ed8d38cb
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-06-30 20:53:54 +0200

    refactor: reorganize Model class methods for clarity and efficiency refactor: remove redundant DiffParser instantiation in get_active_file_content and get_list_of_categories methods

--- source/model/model.py
+++ source/model/model.py
@@ -5,8 +5,13 @@ from source.model.diff_parser import DiffParser
 class Model:
     def __init__(self):
         self.file_parser = FileParser()
+        self.diff_parser = None
         self.active_file = None
-
+        
+        self.content = []
+        self.categories = []
+        self.header_by_category = {}
+        self.content_by_header = {}
 
     def get_files(self, source_directory):
         self.file_parser.set_source_dir(source_directory)
@@ -15,28 +20,31 @@ class Model:
         
     def set_active_file(self, file_name):
         self.active_file = file_name
+        self.diff_parser = DiffParser(self.active_file)
+        self.diff_parser.parse_diffs()
+        
+        self.content = self.diff_parser.get_diffs()
+        self.categories = self.diff_parser.get_categories()
+        self.header_by_category = {}
+        self.content_by_header = {}
+        
+        for category in self.categories:
+            self.header_by_category[category] = self.diff_parser.get_all_headers_with_category(category)
+        
+        for headers in self.header_by_category.values():
+            for header in headers:
+                self.content_by_header[header] = self.diff_parser.filter_diffs_by_header([header])
 
     def get_active_file_content(self):
-        if not self.active_file:
-            return None
-    
-        diff_parser = DiffParser(self.active_file)
-        diff_parser.parse_diffs()
-        return diff_parser.get_diffs()
+        return self.content
 
     def get_list_of_categories(self):
-        if not self.active_file:
-            return None
-        diff_parser = DiffParser(self.active_file)
-        diff_parser.parse_diffs()
-        # diff_parser.debugLevel = 1
-        return diff_parser.get_categories()
+        return self.categories
     
-    def get_category_content(self, category_name):
-        if not self.active_file:
-            return None
-        diff_parser = DiffParser(self.active_file)
-        diff_parser.parse_diffs()
-        # diff_parser.debugLevel = 1
-        return diff_parser.filter_diffs_by_category(category_name)
+    def get_headers_by_category(self, category_name):
+        return self.header_by_category.get(category_name, [])
+    
+    def get_content_by_header(self, header_name):
+        return self.content_by_header.get(header_name, [])
+  
     
