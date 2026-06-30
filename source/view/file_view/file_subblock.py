bd97f68cf74ccd3be01a50a7b9709c585da980e7
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-06-30 20:54:04 +0200

    refactor: update header label and method names for clarity and consistency refactor: modify content addition logic to handle list of strings

--- source/view/file_view/file_subblock.py
+++ source/view/file_view/file_subblock.py
@@ -8,14 +8,19 @@ class FileSubBlock(ctk.CTkFrame):
         self.create_widgets()
         
     def create_widgets(self):
-        self.category_header = ctk.CTkLabel(self, text="", font=("Arial", 16, "bold"))
-        self.category_header.pack(padx=10, pady=10, fill="x")
+        self.header_name = ctk.CTkLabel(self, text="", font=("Arial", 24, "bold"))
+        self.header_name.pack(padx=10, pady=10, fill="x")
         
-        self.content_frame = ctk.CTkFrame(self)
+        self.content_frame = ctk.CTkScrollableFrame(self)
         self.content_frame.pack(padx=10, pady=10, fill="both", expand=True)
         
-    def set_category(self, category_name):
-        self.category_header.configure(text=category_name)
+    def set_header(self, header_name):
+        self.header_name.configure(text=header_name)
     
-    def add_content(self, content_widget):
-        content_widget.pack(in_=self.content_frame, padx=5, pady=5, fill="both", expand=True)
+    def add_content(self, content_list):
+        for content_string in content_list:
+            for block in content_string:
+                if block.startswith("#"):
+                    continue
+                content_widget = ctk.CTkLabel(self.content_frame, text=block, anchor="center", justify="left", wraplength=400, font=("Arial", 18))
+                content_widget.pack(padx=5, pady=5, fill="both", expand=True)
